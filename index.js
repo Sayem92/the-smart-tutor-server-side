4839435484abfd8748c055b1190c0a57aacf6878
Sayem92 <sayemmiha123@gmail.com>
2022-10-27 12:37:16 +0600

    update data

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const port = process.env.PORT || 5000;
 const tutorials = require('./data/courses.json')
 
 app.get('/', (req, res) =>{
-    res.send('Programming Learner server is running now')
+    res.send('The Smart Tutor server is running now')
 })
 
 app.get('/tutorials', (req, res) =>{
     res.send(tutorials)
 })
+
+
 
 app.get('/tutorials/:id', (req, res) =>{
     const id= req.params.id;
@@ -30,5 +32,5 @@ app.get('/tutorials/detailsPage/:id', (req, res) =>{
 
 
 app.listen(port, ()=>{
-    console.log('server running port is ', port);
+    console.log('server running port on ', port);
 })
